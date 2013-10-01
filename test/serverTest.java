e77e1c7f70101420285ccb3b122c23bae1c1e937
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-10-01 15:31:41 -0500

    Refactors tests

--- test/serverTest.java
+++ test/serverTest.java
@@ -8,77 +8,79 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Random;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class serverTest {
 
     private Server server;
     private Random rand = new Random();
-    private Integer port;
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
 
     @Before
     public void setUp() throws IOException {
-        port = rand.nextInt((6000 - 2000) + 1) + 2000;
+        Integer port = rand.nextInt((6000 - 2000) + 1) + 2000;
         server = new Server(port);
-        try{
-          clientSocket = new Socket("localhost", port);
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        clientSocket = trySocketCreation(port);
         out = new PrintWriter(clientSocket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
     }
 
-    @After
+    private Socket trySocketCreation(Integer port) {
+        try{
+          clientSocket = new Socket("localhost", port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return clientSocket;
+    }
+
+    private String readResponse() {
+        String response = "";
+        String nextLine;
+        try {
+            while ((nextLine = in.readLine()) != null){
+                    response += nextLine;
+                }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+
+    }    @After
     public void tearDown(){
         server.close();
+        tryClientClose();
     }
 
-    @Test
-    public void testServerResponseStatus(){
-
-        try{
-
-            out.print("GET /ping HTTP/1.0\r\n\r\n");
-            server.respond();
-            String serverResponse = in.readLine();
+    private void tryClientClose() {
+        try {
             clientSocket.close();
-            assertEquals("HTTP/1.0 200 OK", serverResponse);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     @Test
-    public void testServerResponseContentType(){
+    public void testResponseStatus(){
 
-        try{
+        out.print("GET /ping HTTP/1.0\r\n\r\n");
+        server.respond();
+        String serverResponse = readResponse();
+        assertTrue(serverResponse.startsWith("HTTP/1.0 200 OK"));
+    }
 
-            out.print("GET /ping HTTP/1.0\r\n\r\n");
-            server.respond();
-            String serverResponse = "";
-            String nextLine;
-            while ((nextLine = in.readLine()) != null){
-                serverResponse += nextLine;
-            }
-            clientSocket.close();
-            assertTrue(serverResponse.contains("Content-type: text/html"));
+    @Test
+    public void testResponseContentType(){
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        out.print("GET /ping HTTP/1.0\r\n\r\n");
+        server.respond();
+        String serverResponse = readResponse();
+        tryClientClose();
+        assertTrue(serverResponse.contains("Content-type: text/html"));
     }
 
 }
