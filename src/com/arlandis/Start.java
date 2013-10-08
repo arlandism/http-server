59c6da7f581180d2c592ef14c5115538704735d9
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-10-08 15:57:11 -0500

    Groundwork for multi-threading

--- src/com/arlandis/Start.java
+++ src/com/arlandis/Start.java
@@ -1,5 +1,10 @@
 package com.arlandis;
 
+import com.sun.corba.se.spi.orbutil.threadpool.ThreadPool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Start{
 
     public static void main(String[] args){
@@ -7,7 +12,7 @@ public class Start{
         Integer port = portNum(args);
         Server server = new Server(port);
         System.out.printf("Server starting on port: %d\n", port);
-
+        ExecutorService threadPool = Executors.newFixedThreadPool(5);
         while (true){
             server.respond();
         }
