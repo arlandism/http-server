a94c23e77ffe1fe66264e98379554d14bc39610f
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-10-08 15:56:01 -0500

    Adds test for 'sleepTime'

--- test/RequestTest.java
+++ test/RequestTest.java
@@ -11,4 +11,11 @@ public class RequestTest {
         Request request = new Request(rawHeader);
         assertEquals(Integer.valueOf(15), request.bytesToRead());
     }
+
+    @Test
+    public void testSleepTime(){
+        String rawHeader = "GET /ping?sleep=16\r\n";
+        Request request = new Request(rawHeader);
+        assertEquals(Integer.valueOf(16), request.sleepTime());
+    }
 }
