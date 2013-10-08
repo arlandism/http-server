e042de52c4242a7c868b5130118810a2e8ad5662
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-10-08 10:28:05 -0500

    Refactors ResponseBuilder

--- src/com/arlandis/ResponseBuilder.java
+++ src/com/arlandis/ResponseBuilder.java
@@ -2,10 +2,10 @@ package com.arlandis;
 
 public class ResponseBuilder {
 
-    private String requestHeader;
+    private RequestInterface requestToRespondTo;
 
-    public ResponseBuilder(String request){
-       requestHeader = request;
+    public ResponseBuilder(RequestInterface request){
+       requestToRespondTo = request;
     }
 
     public String response(){
@@ -19,14 +19,35 @@ public class ResponseBuilder {
                           "<br /><label>bar<input name='bar'></label>" +
                           "<br /><input value='submit' type='submit'></form>";
 
-        if (requestHeader.startsWith("GET /form")){
+        if (requestToRespondTo.headers().startsWith("GET /form")){
             body = formBody;
            }
+        else if (requestToRespondTo.headers().startsWith("POST")){
+            body = formParams();
+        }
         else{
             body = "<html><body>pong</body></html>";
         }
 
         response = head + "\n" + contentType + "\n\r\n" + body;
         return response;
+    }
+
+    private String formParams() {
+        String LINE_BREAK = "<br />";
+        String requestBody = requestToRespondTo.getBody();
+        return "foo = " + fooValue(requestBody) + LINE_BREAK + "bar = " + barValue(requestBody);
+    }
+
+    private String fooValue(String requestBody){
+        Integer fooBegin = requestBody.indexOf("=") + 1;
+        Integer fooEnd = requestBody.indexOf("&");
+        return requestBody.substring(fooBegin, fooEnd);
+    }
+
+    private String barValue(String requestBody){
+        Integer fooEnd = requestBody.indexOf("&");
+        Integer barBegin = requestBody.indexOf("=", fooEnd) + 1;
+        return requestBody.substring(barBegin, requestBody.length());
     }
 }
