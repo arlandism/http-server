a81e3ed4dc0dd086129a20a90487720c7c572ee6
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-10-09 14:17:26 -0500

    Removes unused imports

--- src/com/arlandis/Start.java
+++ src/com/arlandis/Start.java
@@ -1,10 +1,5 @@
 package com.arlandis;
 
-import com.sun.corba.se.spi.orbutil.threadpool.ThreadPool;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class Start{
 
     public static void main(String[] args){
@@ -12,7 +7,7 @@ public class Start{
         Integer port = portNum(args);
         Server server = new Server(port);
         System.out.printf("Server starting on port: %d\n", port);
-        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+
         while (true){
             server.respond();
         }
