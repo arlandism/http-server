63abe68526b6e2cf127fda8d8d528b66b6525f9c
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-10-08 21:08:30 -0500

    Refactors 'ResponseBuilder'

--- src/com/arlandis/ResponseBuilder.java
+++ src/com/arlandis/ResponseBuilder.java
@@ -7,28 +7,21 @@ public class ResponseBuilder {
 
     private RequestInterface request;
 
-    public ResponseBuilder(RequestInterface request){
-       this.request = request;
+    public ResponseBuilder(RequestInterface request) {
+        this.request = request;
     }
 
-    public String response(){
+    public String response() {
         String response;
         String head = "HTTP/1.0 200 OK";
         String contentType = "Content-type: text/html";
         String body;
-        String formBody = "<html><body>" +
-                          "<form method='post', action='/form'>" +
-                          "<label>foo<input name='foo'></label>" +
-                          "<br /><label>bar<input name='bar'></label>" +
-                          "<br /><input value='submit' type='submit'></form>";
 
-        if (request.headers().startsWith("GET /form")){
-            body = formBody;
-           }
-        else if (request.headers().startsWith("POST")){
+        if (request.headers().startsWith("GET /form")) {
+            body = formBody();
+        } else if (request.headers().startsWith("POST ")) {
             body = formParams();
-        }
-        else{
+        } else {
             body = "<html><body>pong</body></html>";
         }
 
@@ -36,33 +29,26 @@ public class ResponseBuilder {
         return response;
     }
 
+    private String formBody(){
+        return "<html><body>" +
+               "<form method='post', action='/form'>" +
+               "<label>foo<input name='foo'></label>" +
+               "<br /><label>bar<input name='bar'></label>" +
+               "<br /><input value='submit' type='submit'></form>";
+    }
+
     private String formParams() {
         String LINE_BREAK = "<br />";
         return "foo = " + decodeValue(request.fooValue()) + LINE_BREAK + "bar = " + decodeValue(request.barValue());
     }
 
-  //  fooValue(requestBody)
-    //barValue(requestBody)
-
-    private String fooValue(String requestBody){
-        Integer fooBegin = requestBody.indexOf("=") + 1;
-        Integer fooEnd = requestBody.indexOf("&");
-        return decodeValue(requestBody.substring(fooBegin, fooEnd));
+    private String decodeValue(String value) {
+        try {
+            return URLDecoder.decode(value, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return "";
+        }
     }
-
-    private String barValue(String requestBody){
-        Integer fooEnd = requestBody.indexOf("&");
-        Integer barBegin = requestBody.indexOf("=", fooEnd) + 1;
-        return decodeValue(requestBody.substring(barBegin, requestBody.length()));
-    }
-
-   private String decodeValue(String value) {
-       try {
-           return URLDecoder.decode(value, "UTF-8");
-       } catch (UnsupportedEncodingException e) {
-           e.printStackTrace();
-           return "";
-       }
-   }
 
 }
