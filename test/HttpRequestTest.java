ba67a651dd7ac835b069d5919a0130e665b65a42
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-10-13 13:38:03 -0500

    Adds more tests for 'HttpRequest'

--- test/HttpRequestTest.java
+++ test/HttpRequestTest.java
@@ -9,12 +9,34 @@ import static org.junit.Assert.assertTrue;
 public class HttpRequestTest {
 
     private MockSleeper sleeper = new MockSleeper();
+    private HttpRequest headlessRequest = new HttpRequest("");
 
     @Test
     public void testReadBytes(){
         String rawHeader = "Content-Length: 15";
         HttpRequest request = new HttpRequest(rawHeader);
         assertEquals(Integer.valueOf(15), request.bytesToRead());
+    }
+
+    @Test
+    public void testHasBody(){
+        String header = "Content-Length";
+        HttpRequest request = new HttpRequest(header);
+        assertTrue(request.hasBody()) ;
+    }
+
+    @Test
+    public void testFooValue(){
+        String body = "foo=hello&bar=hi";
+        headlessRequest.setBody(body);
+        assertEquals("hello", headlessRequest.fooValue());
+    }
+
+    @Test
+    public void testBarValue(){
+        String body = "foo=hello&bar=hi";
+        headlessRequest.setBody(body);
+        assertEquals("hi", headlessRequest.barValue());
     }
 
     @Test
