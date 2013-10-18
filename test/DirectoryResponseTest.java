83f2567e19ba5dbe9a64cf02683855dd63804ef4
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-10-17 22:14:33 -0500

    Adds 'DirectoryResponseTest'

---
+++ test/DirectoryResponseTest.java
@@ -0,0 +1,29 @@
+import com.arlandis.Config;
+import com.arlandis.Responses.FileResponses.DirectoryResponse;
+import com.arlandis.interfaces.ResourceRetriever;
+import mocks.MockFileReader;
+import mocks.MockRequest;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DirectoryResponseTest {
+
+    private MockFileReader mockFileReader = new MockFileReader("", "bar");
+    private ResourceRetriever retriever = mockFileReader;
+    private MockRequest request = new MockRequest("", "me");
+
+    @Test
+    public void testDirectoryResponsePollsReader(){
+        DirectoryResponse response = new DirectoryResponse(request, retriever);
+        assertEquals("bar",response.body());
+    }
+
+    @Test
+    public void testDirectoryResponsePollsConfig(){
+        Config.instance().setRootDir("buzz");
+        DirectoryResponse response = new DirectoryResponse(request, retriever);
+        response.body();
+        assertEquals("buzz/me", mockFileReader.history());
+    }
+}
