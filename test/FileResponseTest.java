ede9a590fc7d0c7e2a76604bbdf84cfee216dad1
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-10-17 21:39:45 -0500

    Adds 'FileResponseTest'

---
+++ test/FileResponseTest.java
@@ -0,0 +1,34 @@
+import com.arlandis.Responses.FileResponses.FileResponse;
+import com.arlandis.interfaces.Request;
+import com.arlandis.interfaces.ResourceRetriever;
+import mocks.MockFileReader;
+import mocks.MockRequest;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FileResponseTest {
+    MockFileReader mockReader = new MockFileReader("data from mock reader");
+    ResourceRetriever retriever = mockReader;
+    MockRequest request = new MockRequest("baz", "buzz");
+
+    class TestFileResponse extends FileResponse{
+
+        public TestFileResponse(Request request, ResourceRetriever retriever){
+            super(request, retriever);
+        }
+
+    }
+
+    private TestFileResponse fileResponse = new TestFileResponse(request, retriever);
+
+    @Test
+    public void testFileResponse(){
+       assertEquals("data from mock reader", fileResponse.body());
+    }
+
+    @Test
+    public void testContentType(){
+        assertEquals(null, fileResponse.contentType());
+    }
+}
