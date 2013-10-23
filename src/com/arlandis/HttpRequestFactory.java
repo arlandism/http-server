2a7a46161584f4e679d0a4a680574a92d4d9c120
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-10-22 22:04:15 -0500

    Finishes refactoring 'HttpRequestFactory'

--- src/com/arlandis/HttpRequestFactory.java
+++ src/com/arlandis/HttpRequestFactory.java
@@ -15,45 +15,44 @@ public class HttpRequestFactory implements RequestFactory {
 
     public HttpRequest nextRequest() throws IOException {
 
-        String requestHeaders = readRequestHeaders(networkIO);
-        HttpRequest request = buildRequest(networkIO, requestHeaders);
+        String requestHeaders = readRequestHeaders();
+        HttpRequest request = buildRequest(requestHeaders);
 
         return request;
     }
 
-    private HttpRequest buildRequest(NetworkIO networkIO, String requestHeaders) throws IOException {
+    private HttpRequest buildRequest(String requestHeaders) throws IOException {
         HttpRequest request = new HttpRequest(requestHeaders);
 
-        setBodyIfPresent(networkIO, request);
+        setBodyIfPresent(request);
         return request;
     }
 
-    private void setBodyIfPresent(NetworkIO networkIO, HttpRequest request) throws IOException {
+    private void setBodyIfPresent(HttpRequest request) throws IOException {
         if (request.hasBody()) {
 
-            setRequestBody(request, networkIO);
+            setRequestBody(request);
 
         }
     }
 
-    private void setRequestBody(HttpRequest request, NetworkIO networkIO) throws IOException {
+    private void setRequestBody(HttpRequest request) throws IOException {
         char[] requestBody = networkIO.read(request.bytesToRead());
         String body = new String(requestBody);
         request.setBody(body);
     }
 
-    private String readRequestHeaders(NetworkIO networkIO) throws IOException {
+    private String readRequestHeaders() throws IOException {
         StringBuilder requestHeaders = new StringBuilder();
         String nextHeader = "bar";
         while (!nextHeader.equals("")) {
-            nextHeader = readNextHeader(networkIO, requestHeaders);
+            nextHeader = readNextHeader(requestHeaders);
         }
         return requestHeaders.toString();
     }
 
-    private String readNextHeader(NetworkIO networkIO, StringBuilder requestHeaders) throws IOException {
-        String nextHeader;
-        nextHeader = networkIO.readLine();
+    private String readNextHeader(StringBuilder requestHeaders) throws IOException {
+        String nextHeader = networkIO.readLine();
         requestHeaders.append(nextHeader);
         return nextHeader;
     }
