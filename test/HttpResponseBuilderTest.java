f6e49dfb6591e74a305152c3f5917ac0d291bbc4
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-11-04 10:16:29 -0600

    Refactors 'HttpResponseBuilderTest'

--- test/HttpResponseBuilderTest.java
+++ test/HttpResponseBuilderTest.java
@@ -1,8 +1,5 @@
 import com.arlandis.*;
-import com.arlandis.Responses.GetFormResponse;
-import com.arlandis.Responses.PongResponse;
-import com.arlandis.Responses.PostFormResponse;
-import com.arlandis.Responses.SleepResponse;
+import com.arlandis.Responses.*;
 import com.arlandis.interfaces.*;
 import mocks.*;
 import org.junit.Before;
@@ -15,6 +12,7 @@ public class HttpResponseBuilderTest {
 
     private MockRequest txtFileRequest;
     private MockRequest serviceRequest;
+    private MockResponse fileResponse;
     private HttpResponseBuilder builder;
     private String testFilePath = "foo/bar/baz.txt";
     private FileResponseFactory factory;
@@ -29,7 +27,8 @@ public class HttpResponseBuilderTest {
     public void setUp() {
         txtFileRequest = new MockRequest("GET /browse/" + testFilePath + " HTTP/1.0\r\n\r\n");
         serviceRequest = new MockRequest("GET /game?1=x&depth=10", "/game?1=x&depth=10");
-        mockFactory = new MockFileResponseFactory(new MockResponse("mock content type", "bar"));
+        fileResponse = new MockResponse("mock content type", "bar");
+        mockFactory = new MockFileResponseFactory(fileResponse);
         factory = mockFactory;
         mockReader = new MockFileReader("foo");
         service = new MockService("foo");
@@ -83,37 +82,28 @@ public class HttpResponseBuilderTest {
         mockFeatureParser.setBrowseValue(true);
         mockFeatureParser.setGameValue(true);
         String response = builder.generateResponse(txtFileRequest, parser);
-        assertContentTypeAndBodyMatch("mock content type", "bar", response);
-        assertFileResponseFactoryCalledCorrectly(txtFileRequest, retriever);
+        assertCorrectResponse(response, fileResponse);
+        assertEquals(txtFileRequest, mockFactory.history()[0]);
+        assertEquals(retriever, mockFactory.history()[1]);
     }
 
     @Test
     public void testDanceWithService() {
         mockFeatureParser.setGameValue(true);
         String response = builder.generateResponse(serviceRequest, parser);
-        assertContentTypeAndBodyMatch("application/json", "foo", response);
+        GameResponse gameResponse = new GameResponse(service, serviceRequest);
+        assertCorrectResponse(response, gameResponse);
     }
 
     @Test
     public void testResponseNotFoundForNonToggledFeatures() {
         mockFeatureParser.setBrowseValue(false);
         String response = builder.generateResponse(txtFileRequest, parser);
-        assertContentTypeAndBodyMatch("text/html",
-                "<html><body>The feature you're looking for can't be found.</body></html>", response);
+        FeatureNotFoundResponse notFoundResponse = new FeatureNotFoundResponse();
+        assertCorrectResponse(response, notFoundResponse);
     }
 
-    private void assertCorrectResponse(String response, Response formResponse) {
-        assertEquals(response, "HTTP/1.0 200 OK" + "\r\n" + "Content-type: " + formResponse.contentType() + "\r\n\r\n" + formResponse.body());
-    }
-
-    private void assertFileResponseFactoryCalledCorrectly(Request request, ResourceRetriever retriever) {
-        assertEquals(request, mockFactory.history()[0]);
-        assertEquals(retriever, mockFactory.history()[1]);
-    }
-
-    private void assertContentTypeAndBodyMatch(String expectedContentType, String expectedBody, String actual) {
-        String OK_STATUS = "HTTP/1.0 200 OK";
-        String contentHeader = "Content-type: ";
-        assertEquals(OK_STATUS + "\r\n" + contentHeader + expectedContentType + "\r\n\r\n" + expectedBody, actual);
+    private void assertCorrectResponse(String responseString, Response response) {
+        assertEquals(responseString, "HTTP/1.0 200 OK" + "\r\n" + "Content-type: " + response.contentType() + "\r\n\r\n" + response.body());
     }
 }
