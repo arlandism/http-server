e6a3d7908603147f3d44c974641aae10a27a42b1
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-11-04 10:20:41 -0600

    Deletes unnecessary 'MockPostRequest'

--- test/ResponseTests/PostFormResponseTest.java
+++ test/ResponseTests/PostFormResponseTest.java
@@ -2,17 +2,18 @@ package ResponseTests;
 
 import com.arlandis.Responses.PostFormResponse;
 import com.arlandis.interfaces.Request;
-import mocks.MockPostRequest;
+import mocks.MockRequest;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class PostFormResponseTest {
 
-    private Request request = new MockPostRequest("POST /form HTTP/1.0", "foo=foo Hello!<>&bar=bar baz<>!");
+    private Request request = new MockRequest("POST /form HTTP/1.0", "/form");
 
     @Test
     public void testPostFormResponseBody() {
+        request.setBody("foo=foo Hello!<>&bar=bar baz<>!");
         PostFormResponse response = new PostFormResponse(request);
         String expectedBody = "foo = foo Hello!<> <br /> bar = bar baz<>!";
         assertEquals(expectedBody, response.body());
