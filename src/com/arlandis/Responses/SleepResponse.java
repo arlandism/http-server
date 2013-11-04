cfab11c5730e4451494dc820e046bb7bb5b1da4e
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-11-04 13:03:48 -0600

    Removes unnecessary, duplicate code

--- src/com/arlandis/Responses/SleepResponse.java
+++ src/com/arlandis/Responses/SleepResponse.java
@@ -4,9 +4,6 @@ import com.arlandis.interfaces.Request;
 import com.arlandis.interfaces.Response;
 import com.arlandis.interfaces.Sleeper;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class SleepResponse implements Response {
 
     private Request request;
@@ -32,14 +29,7 @@ public class SleepResponse implements Response {
 
     private Integer sleepTime() {
         Integer start = request.headers().indexOf("sleep") + 6;
-        return parseHeadersForInt(start);
-    }
-
-    private Integer parseHeadersForInt(Integer indexToStartAt) {
-        Pattern intsOnly = Pattern.compile("\\d+");
-        Matcher matcher = intsOnly.matcher(request.headers().substring(indexToStartAt));
-        matcher.find();
-        String inputInt = matcher.group();
-        return Integer.parseInt(inputInt);
+        Integer end = request.headers().indexOf("HTTP") - 1;
+        return Integer.valueOf(request.headers().substring(start, end));
     }
 }
